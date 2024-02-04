75532c8c29ef9aa5016f60fb76c6f5526f94901f
Nathan Smith <smith.11170@outlook.com>
2024-02-04 01:56:06 -0500

    embrace global variables (for now), set up backbuffer

--- code/sdl_handmade.cpp
+++ code/sdl_handmade.cpp
@@ -21,132 +21,54 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <iostream>
 #include <sstream>
 
-static bool running {true};
-bool handleEvent(SDL_Event *Event);
-void outputSDLErrorAndQuit(std::string caller);
-void outputTTFErrorAndQuit(std::string caller);
-SDL_Texture *getFPSAsTexture(std::chrono::steady_clock::time_point start, long counted_frames,
-                             std::stringstream *time_text, SDL_Renderer *renderer, TTF_Font *font,
-                             SDL_Color *text_color, SDL_Rect *message_rect);
+#define internal static
+#define local_persist static
+#define global_variable static
 
-int main() {
-  int call_code {SDL_Init(SDL_INIT_VIDEO)};
-  if (call_code < 0) {
-    outputSDLErrorAndQuit("SDL_Init");
-    return 1;
-  }
+global_variable bool Running {true};
+global_variable SDL_Texture *Texture {};
+global_variable int BitmapWidth {};
+global_variable int BitmapHeight {};
+global_variable uint8_t *BitmapMemory {};
+global_variable size_t BytesPerPixel {};
+global_variable long CountedFrames {};
+global_variable std::chrono::steady_clock::time_point StartTime {};
+global_variable TTF_Font *Sans {};
+global_variable std::stringstream FpsText {};
+global_variable SDL_Color White {255, 255, 255, 255};
+global_variable SDL_Texture *FpsMessageTexture {};
 
-  call_code = TTF_Init();
-  if (call_code < 0) {
-    outputTTFErrorAndQuit("TTF_Init");
-    return 1;
-  }
+void handleEvent(SDL_Event *event);
+internal void outputSDLError(std::string caller);
+internal void outputTTFError(std::string caller);
+internal SDL_Texture *getFPSTexture(SDL_Renderer *renderer, SDL_Rect *message_rect);
+internal void SDLResizeTexture(SDL_Renderer *renderer, int width, int height);
+internal void SDLUpdateWindow(SDL_Window *Window, SDL_Renderer *Renderer);
 
-  auto window {
-      SDL_CreateWindow("Handmade", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE)};
-  if (window == nullptr) {
-    outputSDLErrorAndQuit("SDL_CreateWindow");
-    return 1;
-  }
-
-  auto renderer {SDL_CreateRenderer(window, -1, 0)};
-  if (renderer == nullptr) {
-    outputSDLErrorAndQuit("SDL_CreateRenderer");
-    return 1;
-  }
-
-  std::stringstream time_text {};
-  TTF_Font *Sans {TTF_OpenFont("fonts/OpenSans-Regular.ttf", 11)};
-  if (Sans == nullptr) {
-    outputTTFErrorAndQuit("TTF_OpenFont");
-    return 1;
-  }
-
-  SDL_Color White {255, 255, 255, 255};
-
-  SDL_Rect fps_rect;
-  fps_rect.x = 0;
-  fps_rect.y = 0;
-
-  // Start counting frames per second
-  long counted_frames = 0;
-  auto fps_timer {std::chrono::steady_clock::now()};
-  SDL_Texture *message_texture {};
-  while (running) {
-    SDL_Event event {};
-    while (SDL_PollEvent(&event)) {
-      handleEvent(&event);
-    }
-
-    int success {SDL_RenderClear(renderer)};
-    if (success < 0) {
-      outputSDLErrorAndQuit("SDL_RenderClear");
-      return 1;
-    }
-
-    if (message_texture != nullptr) {
-      SDL_DestroyTexture(message_texture);
-    }
-    message_texture = getFPSAsTexture(fps_timer, counted_frames, &time_text, renderer, Sans, &White, &fps_rect);
-    if (message_texture != nullptr) {
-      SDL_RenderCopy(renderer, message_texture, NULL, &fps_rect);
-    }
-
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderPresent(renderer);
-
-    ++counted_frames;
-  }
-
-  return 0;
-}
-
-bool handleEvent(SDL_Event *event) {
-  bool should_quit {false};
-
-  switch (event->type) {
-  case SDL_QUIT: {
-    std::cout << "SDL_QUIT\n";
-    running = false;
-  } break;
-
-  case SDL_WINDOWEVENT: {
-    switch (event->window.event) {
-    case SDL_WINDOWEVENT_RESIZED: {
-      std::cout << "SDL_WINDOWEVENT_RESIZED ( " << event->window.data1 << "," << event->window.data2 << ")\n";
-    } break;
-
-    case SDL_WINDOWEVENT_EXPOSED: {
-    } break;
-    }
-  } break;
-  }
-
-  return should_quit;
-}
-
-void outputSDLErrorAndQuit(std::string caller) {
+internal void outputSDLError(std::string caller) {
   const char *err {SDL_GetError()};
   std::cerr << "SDL error in " << caller << ": " << err << '\n';
 }
 
-void outputTTFErrorAndQuit(std::string caller) {
+internal void outputTTFError(std::string caller) {
   const char *err {TTF_GetError()};
   std::cerr << "TTF error in " << caller << ": " << err << '\n';
 }
 
-SDL_Texture *getFPSAsTexture(std::chrono::steady_clock::time_point start, long counted_frames,
-                             std::stringstream *time_text, SDL_Renderer *renderer, TTF_Font *font,
-                             SDL_Color *text_color, SDL_Rect *message_rect) {
-  float fs = std::chrono::duration<float> {std::chrono::steady_clock::now() - start}.count();
-  float avg_fps {static_cast<float>(counted_frames) / fs};
+internal SDL_Texture *getFPSTexture(SDL_Renderer *renderer, SDL_Rect *message_rect) {
+  if (FpsMessageTexture != nullptr) {
+    SDL_DestroyTexture(FpsMessageTexture);
+  }
 
-  time_text->str("");
-  *time_text << "FPS: " << avg_fps;
+  float fs = std::chrono::duration<float> {std::chrono::steady_clock::now() - StartTime}.count();
+  float avg_fps {static_cast<float>(CountedFrames) / fs};
 
-  SDL_Surface *message_surface {TTF_RenderText_Solid(font, time_text->str().c_str(), *text_color)};
+  FpsText.str("");
+  FpsText << "FPS: " << avg_fps;
+
+  SDL_Surface *message_surface {TTF_RenderText_Solid(Sans, FpsText.str().c_str(), White)};
   if (message_surface == nullptr) {
-    outputTTFErrorAndQuit("TTF_RenderText_Solid");
+    outputTTFError("TTF_RenderText_Solid");
     return nullptr;
   }
 
@@ -155,9 +77,126 @@ SDL_Texture *getFPSAsTexture(std::chrono::steady_clock::time_point start, long c
 
   auto result = SDL_CreateTextureFromSurface(renderer, message_surface);
   if (result == nullptr) {
-    outputSDLErrorAndQuit("SDL_CreateTextureFromSurface");
+    outputSDLError("SDL_CreateTextureFromSurface");
     return nullptr;
   }
   SDL_FreeSurface(message_surface);
   return result;
+}
+
+internal void SDLResizeTexture(SDL_Renderer *renderer, int width, int height) {
+  if (Texture != nullptr) {
+    SDL_DestroyTexture(Texture);
+  }
+  if (BitmapMemory != nullptr) {
+    delete BitmapMemory;
+  }
+  Texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, width, height);
+  BitmapWidth = width;
+  BitmapHeight = height;
+  BitmapMemory = new uint8_t[width * height * BytesPerPixel];
+}
+
+internal void SDLUpdateWindow(SDL_Window *window, SDL_Renderer *renderer) {
+
+  SDL_UpdateTexture(Texture, 0, BitmapMemory, BitmapWidth * BytesPerPixel);
+
+  SDL_RenderCopy(renderer, Texture, NULL, NULL);
+
+  SDL_Rect fps_rect;
+  fps_rect.x = 0;
+  fps_rect.y = 0;
+  FpsMessageTexture = getFPSTexture(renderer, &fps_rect);
+  if (FpsMessageTexture != nullptr) {
+    SDL_RenderCopy(renderer, FpsMessageTexture, NULL, &fps_rect);
+  }
+  SDL_RenderPresent(renderer);
+}
+
+void handleEvent(SDL_Event *event) {
+  bool should_quit {false};
+
+  switch (event->type) {
+  case SDL_QUIT: {
+    std::cout << "SDL_QUIT\n";
+    Running = false;
+  } break;
+
+  case SDL_WINDOWEVENT: {
+    switch (event->window.event) {
+    case SDL_WINDOWEVENT_SIZE_CHANGED: {
+      int width {};
+      int height {};
+      SDL_GetWindowSize(SDL_GetWindowFromID(event->window.windowID), &width, &height);
+      SDLResizeTexture(SDL_GetRenderer(SDL_GetWindowFromID(event->window.windowID)), width, height);
+      std::cout << "SDL_WINDOWEVENT_RESIZED ( " << event->window.data1 << "," << event->window.data2 << ")\n";
+    } break;
+
+    case SDL_WINDOWEVENT_FOCUS_GAINED: {
+      printf("SDL_WINDOWEVENT_FOCUS_GAINED\n");
+    } break;
+
+    case SDL_WINDOWEVENT_EXPOSED: {
+      SDL_Window *window = SDL_GetWindowFromID(event->window.windowID);
+      SDL_Renderer *renderer = SDL_GetRenderer(window);
+      SDLUpdateWindow(window, renderer);
+    } break;
+    }
+  } break;
+  }
+}
+
+int main() {
+  int result_code {};
+
+  result_code = SDL_Init(SDL_INIT_VIDEO);
+  if (result_code < 0) {
+    outputSDLError("SDL_Init");
+    return 1;
+  }
+
+  result_code = TTF_Init();
+  if (result_code < 0) {
+    outputTTFError("TTF_Init");
+    return 1;
+  }
+
+  auto window {
+      SDL_CreateWindow("Handmade", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE)};
+  if (window == nullptr) {
+    outputSDLError("SDL_CreateWindow");
+    return 1;
+  }
+
+  auto renderer {SDL_CreateRenderer(window, -1, 0)};
+  if (renderer == nullptr) {
+    outputSDLError("SDL_CreateRenderer");
+    return 1;
+  }
+
+  Sans = TTF_OpenFont("fonts/OpenSans-Regular.ttf", 11);
+  if (Sans == nullptr) {
+    outputTTFError("TTF_OpenFont");
+    return 1;
+  }
+
+  // Start counting frames per second
+  CountedFrames = 0;
+  StartTime = std::chrono::steady_clock::now();
+
+  int width {};
+  int height {};
+  SDL_GetWindowSize(window, &width, &height);
+  SDLResizeTexture(renderer, width, height);
+  while (Running) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      handleEvent(&event);
+    }
+    // RenderWeirdGradient(XOffset, YOffset);
+    SDLUpdateWindow(window, renderer);
+    ++CountedFrames;
+  }
+
+  return 0;
 }
