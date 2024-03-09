ad9a0c506c17d5676d5f8f7d2e852e07dfab23e6
Nathan Smith <smith.11170@outlook.com>
2024-03-09 10:11:46 -0500

    Change initialization method

--- handmade/handmade_impl.cpp
+++ handmade/handmade_impl.cpp
@@ -1,3 +1,19 @@
+/*
+<name TBD>, a handmade game about simulating things in space
+Copyright (C) 2024 Nathan Smith
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 module Handmade;
 import std;
 
@@ -18,7 +34,7 @@ void renderWeirdGradient(GameState *state, GameOffscreenBuffer *buffer) {
         for (int x : std::ranges::iota_view{0, buffer->width}) {
             auto blue{(std::uint8_t)(x + state->blueOffset)};
             auto green{(std::uint8_t)(y + state->greenOffset)};
-            auto bytes = (std::uint32_t)(blue) << 8 | green;
+            auto bytes{(std::uint32_t)(blue) << 8 | green};
 
             buffer->memory[offset] = bytes;
             offset++;
