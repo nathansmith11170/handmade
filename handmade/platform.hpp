e2da4333648e4712eabb6a019261b1f3ead00859
Nathan Smith <smith.11170@outlook.com>
2024-03-12 15:45:10 -0400

    pragma praxis

--- handmade/platform.hpp
+++ handmade/platform.hpp
@@ -14,8 +14,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-#include <string>
 #pragma once
+#include <string>
 
 struct DebugReadFileResult {
     void *data;
